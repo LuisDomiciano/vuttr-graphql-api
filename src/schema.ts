ac9f8f000d207e2fe7bbe0006af495a68b6dd2bd
Luis <luis.domiciano@hotmail.com>
2023-09-17 21:42:50 -0300

    feat: should return a tool

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,24 @@
 import { makeExecutableSchema } from "@graphql-tools/schema";
 
+type Tool = {
+  id: number;
+  title: string;
+  link: string;
+  description: string;
+};
+
+const tools: Tool[] = [
+  {
+    id: 1,
+    title: "Notion",
+    link: "https://notion.so",
+    description:
+      "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.",
+  },
+];
+
 const typeDefinitions = `
   type Query {
-    hello: String!
     tool: Tool!
   }
 
@@ -11,13 +27,18 @@ const typeDefinitions = `
     title: String!
     link: String!
     description: String!
-    tags: [String!]!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Vai Corinthians!",
+    tool: () => tools[0],
+  },
+  Tool: {
+    id: (parent: Tool) => parent.id,
+    title: (parent: Tool) => parent.title,
+    link: (parent: Tool) => parent.link,
+    description: (parent: Tool) => parent.description,
   },
 };
 
