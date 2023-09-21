d980aa369b9acfe3523ea4e95099294a042d858d
Luis <luis.domiciano@hotmail.com>
2023-09-21 10:16:32 -0300

    feat: createContext

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import { createYoga } from "graphql-yoga";
 import { createServer } from "http";
+import { createContext } from "./context";
 import { schema } from "./schema";
 (() => {
   const port = 3000;
-  const yoga = createYoga({ schema });
+  const yoga = createYoga({ schema, context: createContext });
   const server = createServer(yoga);
   server.listen(port, () => console.log("Server is running..."));
 })();
